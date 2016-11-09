8f5b026bb2247676e888ea81a5fcb9e53442f2c6
stas-g <ng414@medschl.cam.ac.uk>
2016-11-09 16:22:38 +0000

    init commit

---
+++ R/lasso_betas.R
@@ -0,0 +1,30 @@
+# LASSO.BETAS (06.08.2014)
+#' Title
+#'
+#' @param x
+#' @param scale
+#' @param nonzero
+#' @param lambdamin
+#' @param names
+#'
+#' @return
+#' @export
+#'
+#' @examples
+
+lasso.betas <- function(x, scale = FALSE, nonzero = TRUE, lambdamin = FALSE, names = FALSE){
+  if(lambdamin == TRUE){
+    b.parse <- coef(x, s = 'lambda.min')
+    betas <- as.vector(b.parse)[-1]
+    names(betas) <- rownames(b.parse)[-1]
+  } else{
+    b.parse <- coef(x, s = 'lambda.1se')
+    betas <- as.vector(b.parse)[-1]
+    names(betas) <- rownames(b.parse)[-1]
+  }
+  betas <- betas[order(abs(betas), decreasing = TRUE)]
+  if(nonzero == TRUE) betas <- betas[betas != 0]
+  if(scale == TRUE) betas <- abs(betas)/sum(abs(betas))
+  if(names == TRUE) return(names(betas))
+  else return(betas)
+}
